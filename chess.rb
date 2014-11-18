04a51c1a52ea2fb87e8c0cdbdb8e3721285a2260
Jake and Wesley <jake+wesley@appacademy.io>
2014-11-18 11:20:51 -0500

    Add class names to files

---
+++ chess.rb
@@ -0,0 +1,27 @@
+# THE STRUCTURE OF THE GAME CODE:
+#
+# Pieces:
+#   Sliding pieces (bishop/rook/queen)
+#     SlidingPiece can implement #moves; each subclass will need #move_dirs
+#   Stepping pieces (knight/king)
+#     SteppingPiece can implement #moves; each subclass will need #deltas
+#   The pawn
+#     Who knows.
+#
+# Methods of Piece:
+#   valid_moves
+#   moves_into_check?(pos)
+#
+# Methods of SlidingPiece:
+#   moves
+#   move_dirs
+#
+# Methods of SteppingPiece:
+#   moves
+#   deltas
+#
+# Methods of Board:
+#   in_check?
+#   move(start, end_pos)
+#     will raise some exceptions under the conditions given
+#   dup
---
+++ king.rb
@@ -0,0 +1,3 @@
+class King < SteppingPiece
+
+end
