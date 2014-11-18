cfc6f9286c1252a5768ff3cb646221fdf5d5bd3d
defseg <6988161+defseg@users.noreply.github.com>
2014-11-18 11:58:58 -0500

    Add some piece logic

--- piece.rb
+++ piece.rb
@@ -1,3 +1,18 @@
 class Piece
 
+  def initialize(pos, color, board)
+    @pos = pos
+    @color = color
+    @board = board
+  end
+
+  def valid_moves
+
+  end
+
+  def is_on_board?(new_pos)
+    #assuming a square board
+    new_pos.all? { |c| (0...board.size).include?(c) }
+  end
+
 end
--- stepping_piece.rb
+++ stepping_piece.rb
@@ -1,3 +1,6 @@
 class SteppingPiece < Piece
-
+  def moves
+    @@deltas.map { |row, col| [pos[0] + row, pos[1] + col] }
+            .select { |new_pos| is_on_board?(new_pos) }
+  end
 end
