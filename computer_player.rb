3b26e4f772d26c286da87f393a5106d287db6e3b
defseg <6988161+defseg@users.noreply.github.com>
2014-11-18 23:49:18 -0500

    Begin computer player

---
+++ computer_player.rb
@@ -0,0 +1,24 @@
+class ComputerPlayer
+  attr_reader :color
+
+  def initialize(color, game_board)
+    @color = color
+    @game_board = game_board
+  end
+
+  def play_turn
+
+  end
+
+  private
+
+    def search_moves(board, counter = 5, search_color = color)
+      if counter == 0
+        # base condition
+      elsif search_color == color
+        # call search_moves, switch color
+      else
+        # call search_moves, switch color, decrement
+      end
+    end
+end
