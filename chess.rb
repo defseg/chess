54a0f174eb1421203d4aa99142ef1ac4aecaca0c
defseg <6988161+defseg@users.noreply.github.com>
2014-11-19 17:48:32 -0500

    Computer player looks for checks

--- chess.rb
+++ chess.rb
@@ -6,22 +6,23 @@ require_relative 'player.rb'
 class Game
   def initialize
     @game_board = Board.new
-    @black, @white = ComputerPlayer.new(:black, @game_board),
+    @black, @white = HumanPlayer.new(:black, @game_board),
                      ComputerPlayer.new(:white, @game_board)
     @turn = :white
   end
 
-  # TODO add variable to check turns -- if you ^C on black's turn, it shouldn't
-  # be white's turn again
+  # TODO should stalemates be treated differently from draws?
 
   def play
     until @game_board.checkmate?(:black) || @game_board.checkmate?(:white) ||
       @game_board.pieces.size == 2
       @game_board.render
       if @turn == :white
+        break if no_valid_moves?(:white)
         @white.play_turn
         @turn = :black
       else
+        break if no_valid_moves?(:black)
         @black.play_turn
         @turn = :white
       end
@@ -35,5 +36,11 @@ class Game
       puts "Draw"
     end
   end
+
+  private
+
+    def no_valid_moves?(color)
+      @game_board.pieces(color).all? { |piece| piece.valid_moves.empty? }
+    end
 
 end
--- computer_player.rb
+++ computer_player.rb
@@ -5,7 +5,7 @@ require_relative 'player.rb'
 class ComputerPlayer < Player
 
   PIECE_VALUES = {
-    King => 10, # if you can either check or take the queen, check
+    King => 1024,
     Queen => 9,
     Rook => 5,
     Bishop => 3,
@@ -25,25 +25,50 @@ class ComputerPlayer < Player
     moves.reject! { |piece, mvs| mvs.nil? || mvs.empty? }
 
     capturing_moves = find_captures(@game_board, moves)
+    checking_moves = find_checks(@game_board, moves)
 
     unless promotable_pawns.empty?
       promotable_pawns.first.promote(Queen)
     end
 
-    if capturing_moves.empty?
-      moves = moves.to_a
-      start_piece = moves.sample
-      start = start_piece[0]
-      end_pos = start_piece[1].sample
-    else
+    # make a first guess to overwrite later
+    moves = moves.to_a
+    start_piece = moves.sample
+    start = start_piece[0]
+    end_pos = start_piece[1].sample
+
+    # look for capturing moves
+    unless capturing_moves.empty?
       start = capturing_moves[0][0]
       end_pos = capturing_moves[0][1]
     end
+
+    # look for checks
+    unless checking_moves.empty?
+      start = checking_moves[0][0]
+      end_pos = checking_moves[0][1]
+    end
+
+    # look for checkmates
 
     @game_board.move(start, end_pos, @color)
   end
 
   private
+
+    def find_checks(board, moves)
+      # make this a proc or something?
+      checking_moves = []
+      moves.each do |start, moves_arr|
+        moves_arr.each do |end_pos|
+          new_board = board.dup.move!(start, end_pos)
+          opp_color = @color == :white ? :black : :white
+          checking_moves << [start, end_pos] if new_board.in_check?(opp_color)
+        end
+      end
+      checking_moves
+
+    end
 
     def find_captures(board, moves)
       capturing_moves = []
@@ -63,7 +88,7 @@ class ComputerPlayer < Player
         end
       end
 
-      capturing_moves.sort!
+      capturing_moves.sort! # this is wrong. it sorts by position, not by value
     end
 
     def board_value(board)
